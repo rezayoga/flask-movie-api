f37f9a4a4ba9f4b69a70b71460093732cb7c6c50
Reza Yogaswara <me@rezayogaswara.com>
2022-01-17 13:41:15 +0700

    Fixing wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
+from flaskvuenginx import app
 import sys
 sys.path.append('/home/rezayogaswara/python_projects')
 sys.path.append('/home/rezayogaswara/python_projects/flaskvuengin')
 
-from flaskvuenginx import app as application
 if __name__ == "__main__":
-   application.run()
+   app.run()
