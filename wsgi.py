cc5dcc1ab30f8455c48de233ee978358ee43a183
Reza Yogaswara <me@rezayogaswara.com>
2022-01-17 13:29:25 +0700

    Update wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append('/home/rezayogaswara/python_projects')
+sys.path.append('/home/rezayogaswara/python_projects/flaskvuengin')
+
 from flaskvuenginx import app
 if __name__ == "__main__":
    app.run()
