7e3374f90252a144625c65706ee1c0dd19919124
Reza Yogaswara <reza.yoga@gmail.com>
2022-01-18 20:19:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from distutils.log import debug
+from forms import TaskForm
+import models
 from dotenv import load_dotenv
 from flask import Flask, render_template, jsonify, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
@@ -15,8 +16,49 @@ db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
-    return "Hello Musa!"
+    tasks = models.Task.query.all()
+
+    if request.headers.get('X-Requested-With') == 'XMLHttpRequest':
+        return jsonify(tasks)
+
+    return render_template('index.html')
+
+
+@app.route('/create', methods=['POST'])
+def create_task():
+    user_input = request.get_json()
+    form = TaskForm(data=user_input)
+
+    if form.validate():
+        task = models.Task(title=form.title.data)
+        db.session.add(task)
+        db.session.commit()
+        return jsonify(task)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete_task():
+    task_id = request.get_json().get('id')
+    task = models.Task.query.filter_by(id=task_id).first()
+
+    db.session.delete(task)
+    db.session.commit()
+
+    return jsonify({'result': 'Ok'}), 200
+
+
+@app.route('/complete', methods=['POST'])
+def complete_task():
+    task_id = request.get_json().get('id')
+    task = models.Task.query.filter_by(id=task_id).first()
+    task.completed = True
+    db.session.add(task)
+    db.session.commit()
+
+    return jsonify({'result': 'Ok'}), 200
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run()
