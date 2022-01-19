24dde003400f46ceb4f75fb54051b83c5c4c5748
Reza Yogaswara <reza.yoga@gmail.com>
2022-01-19 09:05:43 +0700

    Updating

---
+++ app/application.py
@@ -0,0 +1,22 @@
+
+from forms import TaskForm
+from dotenv import load_dotenv
+from flask import Flask, render_template, jsonify, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+from config import Config
+
+load_dotenv('./.flaskenv')
+
+db = SQLAlchemy()
+
+
+def create_app(app_name="Flask Vue"):
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
+
+    from . import routes
+    db.create_all()
+
+    return app
--- app/models.py
+++ app/models.py
@@ -2,7 +2,6 @@ from dataclasses import dataclass
 from datetime import datetime
 from . import db
 
-
 @dataclass
 class Task(db.Model):
 
