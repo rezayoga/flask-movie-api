426f70223a0b3de1643a0eb7c4aae8ef1bacf072
Reza Yogaswara <reza.yoga@gmail.com>
2022-01-20 20:39:07 +0700

    Updating

---
+++ config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+BASE_DIR = Path(__file__).parent
+print(BASE_DIR)
+
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        str(BASE_DIR.joinpath('db.sqlite'))
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'R3z4y0gasw@R4'
