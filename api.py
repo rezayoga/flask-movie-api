bee258055b58c2fa2398f60143c9b8ca1282ff7b
Reza Yogaswara <reza.yoga@gmail.com>
2022-01-20 23:19:48 +0700

    Updating

--- api.py
+++ api.py
@@ -53,7 +53,7 @@ class Movie(db.Model):
     created_at: str
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('tb_users.id'), nullable=False)
     genre = db.Column(db.String(64), nullable=False)
     title = db.Column(db.String(256), nullable=False)
     directors = db.Column(db.String(256), nullable=False)
