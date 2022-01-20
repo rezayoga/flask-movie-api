f8479234606c8eeea47920c6f69595db63c9a479
Reza Yogaswara <reza.yoga@gmail.com>
2022-01-20 23:00:55 +0700

    Updating

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ print(BASE_DIR)
 
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        str(BASE_DIR.joinpath('db.sqlite'))
+    #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+    #    str(BASE_DIR.joinpath('db.sqlite'))
+    
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:rezareza1985@localhost/db_movie'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'R3z4y0gasw@R4'
